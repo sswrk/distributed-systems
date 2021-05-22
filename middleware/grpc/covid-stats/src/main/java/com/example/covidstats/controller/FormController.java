25d5266d68989f98a4e5c54499a73464a8114c2c
sswrk <swierk.szymon99@gmail.com>
2021-05-22 18:02:30 +0200

    created form

---
+++ middleware/grpc/covid-stats/src/main/java/com/example/covidstats/controller/FormController.java
@@ -0,0 +1,7 @@
+package com.example.covidstats.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FormController {
+}
---
+++ middleware/grpc/covid-stats/src/test/java/com/example/covidstats/CovidStatsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.covidstats;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CovidStatsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
